0bc857dbcf448b7fc678ee5af19df118d4028ed9
Daul <daulso@naver.com>
2019-11-13 10:45:06 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express(); 
+var runWandbox = require('wandbox-api');
+
+runWandbox.fromString('console.log(1+1);',{'compiler':'nodejs-head'},function clbk(error, results){
+	if(error){
+		throw new Error(error.message);
+	}
+	var out=results;
+	console.log(out);
+	app.get('/',function(req,res){
+		res.send(out);
+	});
+});
+
+app.listen(3000, function(){
+	console.log('Connected!');
+});
+
