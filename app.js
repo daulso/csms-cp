b62d58e2540ca69474af98115a907091a9f81f9c
daulso <daulso@naver.com>
2019-12-06 15:55:32 +0900

    add compile-run module

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express(); 
 var runWandbox = require('wandbox-api');
 
+const {c, cpp, node, python, java} = require('compile-run');
+
 runWandbox.fromString('console.log(1+1);',{'compiler':'nodejs-head'},function clbk(error, results){
 	if(error){
 		throw new Error(error.message);
@@ -13,8 +15,32 @@ runWandbox.fromString('console.log(1+1);',{'compiler':'nodejs-head'},function cl
 		res.send(response['program_output']);
 	});
 });
+/*
+const sourcecode = `print("Hell0 W0rld!")`;
+let resultPromise = python.runSource(sourcecode);
+resultPromise
+    .then(result => {
+	            console.log(result);
+	        })
+    .catch(err => {
+	            console.log(err);
+	        });
+*/
+java.runSource(`public class HelloWorld{
+			public static void main(String []args){
+		             System.out.println("Hello World");
+		        }}`, (err, result) => {
+	    if(err){
+		            console.log(err);
+		        }
+	    else{
+		            console.log(result);
+		        }
+});
+
 
 app.listen(3000, function(){
 	console.log('Connected!');
 });
+
 
